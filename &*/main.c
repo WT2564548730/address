584f68c417505eaf924887868addaf900847c53f
Tao2564548730 <2564548730@qq.com>
2019-09-24 17:43:54 +0800

    Update main.c

--- &*/main.c
+++ &*/main.c
@@ -1,15 +1,21 @@
-//
-//  main.c
-//  &*
-//
-//  Created by W20181105881 on 2019/9/24.
-//  Copyright © 2019 W20181105881. All rights reserved.
-//
-
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#include <stdlib.h>
+int main(){
+    int i=5;
+    int *p=&i;
+    char c=(char*)(i);
+    char a[8]="efbff53c";
+    printf("p = %x\n(char)(i) = %d\n(char*)(i) = %d\n(*)(efbff53c) = %d\n",p,(char)(i),(char*)(i),*("efbff53c"));
+    if((char)(i)==(char*)(i)){
+        printf("ture\n");
+    }
+    else{
+        printf("false\n");
+    }
+    printf("a[0] = %x\n&a = %x\na = %x\n",a[0],&a,a);
+    int x=65;
+    printf("(char)('65') = %c\n(char)(65) = %c\n(int)(A) = %d\n(int)(e) = %d\n",(char)('65'),(char)(65),(int)('A'),(int)('e'));
+    
+    free(p);
+    free(a);
 }
